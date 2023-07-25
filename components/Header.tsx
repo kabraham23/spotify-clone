a4ea3450a4e2266d3d56eb21f69982684d6ae57f
Katie A <katie.t.abraham@gmail.com>
2023-07-25 14:51:37 -0500

    log out and log in working

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
 "use client"
 
+import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-
-import Button from "./Button";
-import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import { toast } from 'react-hot-toast';
 
 
 interface HeaderProps {
@@ -22,8 +26,18 @@ const Header: React.FC<HeaderProps> = ({
     const authModal = useAuthModal();
     const router = useRouter();
 
-    const handleLogout = () => {
-        //handle logout
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+        router.refresh();
+
+        if (error) {
+            toast.error(error.message);
+        } else {
+            toast.success('Logged out!')
+        }
     }
     return (
         <div 
@@ -119,6 +133,22 @@ const Header: React.FC<HeaderProps> = ({
                 items-center
                 gap-x-4"
                 >
+                    {user ? (
+                        <div className="flex gap-x-4 items-center">
+                            <Button
+                                onClick={handleLogout}
+                                className="bg-white px-6 py-2"
+                            >
+                                Logout
+                            </Button>
+                            <Button
+                                onClick={() => router.push('/account')}
+                                className="bg-white"
+                            >
+                                <FaUserAlt />
+                            </Button>
+                        </div>
+                    ) : (
                     <>
                     <div>
                         <Button
@@ -144,6 +174,7 @@ const Header: React.FC<HeaderProps> = ({
                         </Button>
                     </div>
                     </>
+                    )}
                 </div>
             </div>
             {children}
