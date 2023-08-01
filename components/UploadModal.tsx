83ff24d031c35fa311402548b0b681329dff5d21
Katie A <katie.t.abraham@gmail.com>
2023-08-01 14:15:54 -0500

    able to upload song

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -11,12 +11,14 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useUser } from "@/hooks/useUser";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
 
 const UploadModal = () => {
     const uploadModal = useUploadModal();
     const [isLoading, setIsLoading] = useState(false);
     const { user } = useUser();
     const supabaseClient = useSupabaseClient();
+    const router = useRouter();
 
     const {
         register,
@@ -64,7 +66,41 @@ const UploadModal = () => {
                 setIsLoading(false);
                 return toast.error('Song failed to upload.')
             }
+            const {
+                data: imageData,
+                error: imageError,
+            } = await supabaseClient
+            .storage
+            .from('images')
+            .upload(`image-${values.title}-${uniqueID}`, imageFile, {
+                cacheControl: '3600',
+                upsert: false
+            });
+            if (imageError) {
+                setIsLoading(false);
+                return toast.error('Image failed to upload.');
+            }
 
+            const {
+                error: supabaseError
+            } = await supabaseClient
+            .from('songs')
+            .insert({
+                user_id: user.id,
+                title: values.title,
+                author: values.author,
+                image_path: imageData.path,
+                song_path: songData.path
+            });
+            if (supabaseError) {
+                setIsLoading(false);
+                return toast.error(supabaseError.message);
+            }
+            router.refresh();
+            setIsLoading(false);
+            toast.success('Song created!');
+            reset();
+            uploadModal.onClose();
         } catch (error) {
             toast.error("something went wrong")
         } finally {
