ce82aef71a3b7fc97c5bb762491dbc14d362b5a6
Katie A <katie.t.abraham@gmail.com>
2023-08-01 15:30:04 -0500

    songs loading to homepage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,12 @@
 import Header from "@/components/Header"
 import ListItem from "@/components/ListItem"
+import getSongs from "@/actions/getSongs";
+import PageContent from "./components/PageContent";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="
     bg-neutral-900
@@ -42,7 +47,7 @@ export default function Home() {
           </h1>
         </div>
         <div>
-          List of Songs
+          <PageContent songs={songs} />
         </div>
       </div>
       </div>
